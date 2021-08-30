fb0ac972569a81536cd0644c7bce8abd78241ee4
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2021-08-30 12:48:31 +0300

    Update Rope.hpp

--- Lib/Rope.hpp
+++ Lib/Rope.hpp
@@ -3,8 +3,7 @@
 
 std::mt19937 rope_rng;
 
-
-template<typename T>
+template <typename T>
 struct rope {
   /* by Wgmlgz */
   struct node {
@@ -13,19 +12,19 @@ struct rope {
     int priority;
     int size = 1;
     T val;
+
     node(const T& _val) : val(_val) { priority = rope_rng(); };
-    ~node(){
+
+    ~node() {
       if (lhs) delete lhs;
       if (rhs) delete rhs;
     }
   };
 
-  int node_size(node* nd) {
-    return nd ? nd->size : 0;
-  }
-  void recalc(node* nd) {
-    if (nd)
-      nd->size = node_size(nd->lhs) + node_size(nd->rhs) + 1;
+  int node_size(node* nd) { return nd ? nd->size : 0; }
+
+  virtual void update(node* nd) {
+    if (nd) nd->size = node_size(nd->lhs) + node_size(nd->rhs) + 1;
   }
 
   void merge(node*& res, node* lhs, node* rhs) {
@@ -36,11 +35,12 @@ struct rope {
     else
       merge(lhs->rhs, lhs->rhs, rhs), res = lhs;
 
-    recalc(res);
+    update(res);
   }
 
   void split(node* nd, node*& lhs, node*& rhs, int pos, int add = 0) {
-    if (!nd) lhs = rhs = nullptr;
+    if (!nd)
+      lhs = rhs = nullptr;
     else {
       int cur = add + node_size(nd->lhs);
       if (cur < pos) {
@@ -48,12 +48,13 @@ struct rope {
       } else {
         split(nd->lhs, lhs, nd->lhs, pos, add), rhs = nd;
       }
-      recalc(nd);
+      update(nd);
     }
-    
   }
 
   node* root = nullptr;
+  rope() {}
+  rope(node* _root) : root(_root) {}
 
   void node_to_vec(std::vector<T>& v, node* nd) {
     if (!nd) return;
@@ -63,10 +64,8 @@ struct rope {
     node_to_vec(v, nd->rhs);
   }
 
-public:
-  size_t size() {
-    return node_size(root);
-  }
+ public:
+  size_t size() { return node_size(root); }
   void push_front(const T& val) {
     auto nd = new node(val);
     merge(root, nd, root);
@@ -87,9 +86,7 @@ public:
     bro.root = nullptr;
   }
   /* borrowing ownership */
-  void operator+=(rope& bro) {
-    push_back(bro);
-  }
+  void operator+=(rope& bro) { push_back(bro); }
   /* borrowing ownership */
   void operator=(rope& bro) {
     delete root;
@@ -135,20 +132,17 @@ public:
   }
 
   rope cut(int l, int r) {
-    if (r >= size()) return cut(l);
+    l = max<int>(0, l);
+    r = min<int>(size(), r);
+
     assert(l < r);
 
-    rope res;
-
-    node* a, * b, * c;
-
+    node *a, *b, *c;
     split(root, a, b, l);
     split(b, b, c, r - l);
-    
     merge(root, a, c);
-    res.root = b;
 
-    return res;
+    return rope(b);
   }
 
   T erase(int pos) {
@@ -156,12 +150,42 @@ public:
     return cutted.to_vec().front();
   }
 
-  void clear() {
-    delete root;
-  }
+  void clear() { delete root; }
+  ~rope() { clear(); }
+};
 
-  /* broken maybe fix later */
-  ~rope() {
-    // delete root;
+template <typename T>
+struct rope_segment {
+  T cur;
+  T merged;
+  int pos;
+};
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const rope_segment<T>& seg) {
+  return os << "{cur: " << seg.cur << " merged: " << seg.merged << " pos: " << seg.pos << "}";
+}
+template <typename T = int, typename CMP = std::less<T>>
+struct ropemin : public rope<rope_segment<int>> {
+  virtual void update(node* nd) override {
+    if (!nd) ret;
+    nd->size = node_size(nd->lhs) + node_size(nd->rhs) + 1;
+
+    nd->val.merged = nd->val.cur;
+    nd->val.pos = node_size(nd->lhs);
+
+    if (nd->lhs and CMP{}(nd->lhs->val.merged, nd->val.merged)) {
+      nd->val.merged = nd->lhs->val.merged;
+      nd->val.pos = nd->lhs->val.pos;
+    }
+    if (nd->rhs and CMP{}(nd->rhs->val.merged, nd->val.merged)) {
+      nd->val.merged = nd->rhs->val.merged;
+      nd->val.pos = nd->rhs->val.pos + node_size(nd->lhs) + 1;
+    }
+  };
+
+  rope_segment<T> get() {
+    assert(root != nullptr);
+    return root->val;
   }
 };
