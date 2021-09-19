18905a4a7635b99d290fe01e0421b9c00cea4d09
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2021-09-19 18:46:56 +0300

    Update Graphs.hpp

--- Lib/Graphs.hpp
+++ Lib/Graphs.hpp
@@ -5,15 +5,14 @@
 #include <iostream>
 #include <utility>
 #include <vector>
+#include <unordered_set>
 
 #include "Dsu.hpp"
 
 #define in std::cin
 
 
-struct edge {
-  int a, b, w;
-};
+struct edge { int a, b, w; };
 bool operator<(const edge& a, const edge& b) { return a.w < b.w; }
 
 std::pair<int, std::vector<std::pair<edge, int>>> id_mst(
@@ -35,6 +34,7 @@ std::pair<int, std::vector<std::pair<edge, int>>> id_mst(
   return {cost, result};
 }
 
+// mst
 std::pair<int, std::vector<edge>> mst(const std::vector<edge>& edges, int n) {
   int cost = 0;
   std::vector<edge> result, _edges = edges;
@@ -52,6 +52,7 @@ std::pair<int, std::vector<edge>> mst(const std::vector<edge>& edges, int n) {
   return {cost, result};
 }
 
+// make dfs easier
 void reduceTree(std::vector<std::vector<int>>& g, int v = -1) {
   static std::vector<int8_t> used;
 
@@ -73,6 +74,7 @@ void reduceTree(std::vector<std::vector<int>>& g, int v = -1) {
   g[v] = t;
 }
 
+// unweighted graph read
 std::vector<std::vector<int>> readG(int n, int m) {
   std::vector<std::vector<int>> graph(n);
 
@@ -87,6 +89,7 @@ std::vector<std::vector<int>> readG(int n, int m) {
   return graph;
 }
 
+// weighted graph read
 std::vector<std::vector<std::pair<int, int>>> readGW(int n, int m) {
   std::vector<std::vector<std::pair<int, int>>> graph(n);
 
@@ -100,3 +103,106 @@ std::vector<std::vector<std::pair<int, int>>> readGW(int n, int m) {
 
   return graph;
 }
+
+// top sort
+void top_sort_dfs(const std::vector<std::vector<int>>& g, std::vector<int>& used, std::vector<int>& ans, int v) {
+  if (used[v]) return;
+  used[v] = 1;
+
+  for (auto& i : g[v])
+    top_sort_dfs(g, used, ans, i);
+
+  ans.push_back(v);
+}
+std::vector<int> top_sort(const std::vector<std::vector<int>>& g) {
+  int n = g.size();
+
+  std::vector<int> used(n), ans;
+  for (int i = 0; i < n; ++i)
+    top_sort_dfs(g, used, ans, i);
+  
+  reverse(begin(ans), end(ans));
+  return ans;
+}
+
+// strong components
+void strong_comps_dfs(const std::vector<std::vector<int>>& g, std::vector<int>& used, int id, int v) {
+  if (used[v]) return;
+  used[v] = id;
+
+  for (auto& i : g[v]) strong_comps_dfs(g, used, id, i);
+}
+std::pair<std::vector<int>, int> strong_comps(const std::vector<std::vector<int>>& g, const std::vector<int>& order) {
+  int n = g.size();
+
+  std::vector<int> ans(n);
+
+  int id = 1;
+
+  for (int i = 0; i < n; ++i) {
+    if (ans[order[i]]) continue;
+    strong_comps_dfs(g, ans, id, order[i]);
+    ++id;
+  }
+  return {ans, id-1};
+}
+
+std::vector<std::vector<int>> makeg_by_comps(std::vector<int> comps, int n, const std::vector<std::pair<int, int>>& edges) {
+  for (auto& i : comps) --i;
+
+  std::vector<std::vector<int>> g(n);
+
+  for (auto [a, b] : edges) {
+    if (comps[a] == comps[b]) continue;
+    g[comps[a]].push_back(comps[b]);
+  }
+
+  return g;
+}
+
+// cutpoints
+struct cutpoints {
+  std::vector<std::vector<int>> g;
+  std::vector<int> used;
+  std::vector<int> fup, tin;
+  std::vector<int> cutpoints;
+  int timer = 0;
+  void find_cutpoints(int v, int p = -1) {
+    used[v] = 1;
+    tin[v] = fup[v] = timer++;
+    int childs = 0;
+    for (auto& i : g[v]) {
+      if (i == p) continue;
+      if (used[i]) {
+        fup[v] = std::min(fup[v], tin[i]);
+      } else {
+        find_cutpoints(i, v);
+        fup[v] = std::min(fup[v], fup[i]);
+        if (fup[i] >= tin[v] and p != -1) {
+          cutpoints.push_back(v);
+        } 
+        ++childs;
+      }
+    }
+
+    if (p == -1 and childs > 1) {
+      cutpoints.push_back(v);
+    }
+  }
+};
+
+// euler path
+struct euler_path {
+  std::vector<int> ans;
+  std::vector<std::unordered_multiset<int>> g;
+  void euler(int v) {
+    while (g[v].size()) {
+      int to = *begin(g[v]);
+      g[v].erase(begin(g[v]));
+      g[to].erase(g[to].find(v));
+
+      euler(to);
+    }
+    ans.push_back(v + 1);
+  }
+};
